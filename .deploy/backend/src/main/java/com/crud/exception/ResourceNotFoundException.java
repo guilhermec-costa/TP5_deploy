dea65f879bda6fa9e68586c6dd9cc018ab30f5cc
Guilherme China <echina725@gmail.com>
2026-03-30 16:02:36 -0300

    remove deploy folder

--- .deploy/backend/src/main/java/com/crud/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.crud.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
